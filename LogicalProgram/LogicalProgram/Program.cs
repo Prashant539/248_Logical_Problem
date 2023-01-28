fe4a6f09cce60b948b260cb52d5991b64605a497
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-29 00:01:06 +0530

    [Prashant]Add: Reverse The Given Number

--- LogicalProgram/LogicalProgram/Program.cs
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -7,7 +7,7 @@ namespace LogicalProgram
         {
             Console.WriteLine("Welcome to Logical Program");
             Console.WriteLine("Please Choose any Option to execute Program");
-            Console.WriteLine("1:FibonacciSeries\n2:PerfectNumber\n3:PrimeNumber");
+            Console.WriteLine("1:FibonacciSeries\n2:PerfectNumber\n3:PrimeNumber\n4:ReverseNumber");
             int Options = Convert.ToInt32(Console.ReadLine());
             switch(Options)
             {
@@ -19,6 +19,9 @@ namespace LogicalProgram
                     break;
                 case 3:
                     PrimeNumber.PrimeNumberPrint();
+                    break;
+                case 4:
+                    ReverseNumber.ReverseNumberPrint();
                     break;
             }
             
---
+++ LogicalProgram/LogicalProgram/ReverseNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class ReverseNumber
+    {
+        public static void ReverseNumberPrint()
+        {
+            int result = 0, reminder;
+            Console.WriteLine("Enter the Number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            while(number!=0)
+            {
+                reminder = number % 10;
+                result = result * 10 + reminder;
+                number = number / 10;
+
+            }
+            Console.WriteLine("Reversed Number: " +result);
+          
+
+        }
+        
+    }
+}
