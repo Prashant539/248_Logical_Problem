4d841eb4e6058c20f52d321a793293e6cd779d34
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-28 23:12:42 +0530

    [Prashant]Add: Perfect Number Program

---
+++ LogicalProgram/LogicalProgram/PerfectNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class PerfectNumber
+    {
+        public static void PerfectNumberPrint()
+        {
+            int  sum = 0;
+            Console.WriteLine("Enter the Number to check Perfect Number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i < number; i++)
+            {
+                if(number % i == 0)
+                {
+                    sum = sum + i;
+                    Console.WriteLine("{0}", i);
+                }
+            }
+            Console.WriteLine("Sum of Divisor are {0}", sum);
+            if ( sum == number)
+            {
+                Console.WriteLine("Number is a Perfect number");
+            }
+            else
+            {
+                Console.WriteLine("Number is not a Perfect number");
+            }
+
+        }
+    }
+}
--- LogicalProgram/LogicalProgram/Program.cs
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -7,12 +7,15 @@ namespace LogicalProgram
         {
             Console.WriteLine("Welcome to Logical Program");
             Console.WriteLine("Please Choose any Option to execute Program");
-            Console.WriteLine("1:FibonacciSeries");
+            Console.WriteLine("1:FibonacciSeries\n2:PerfectNumber");
             int Options = Convert.ToInt32(Console.ReadLine());
             switch(Options)
             {
                 case 1:
                     FibonacciSeries.Fibonacci();
+                    break;
+                case 2:
+                    PerfectNumber.PerfectNumberPrint();
                     break;
             }
             
