978d10949cc03c52c9412cd934b097470ddb01e7
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-02-01 23:17:29 +0530

    [Prashant]Add: Calculate Monthly Payment

---
+++ LogicalProgram/LogicalProgram/MonthlyPayment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class MonthlyPayment
+    {
+        public static void Payment()
+        {
+            int p, n, y;
+            double payment, r, Rate;
+            Console.WriteLine("Enter the Principal Amount : ");
+            p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the No.of Years : ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Rate of Interest : ");
+            Rate = Convert.ToDouble(Console.ReadLine());
+            n = 12 * y;
+            r = Rate / (12 * 100);
+            payment = (p * r) / (1 - (Math.Pow((1 + r), (-n))));
+            Console.WriteLine("Monthly Payment : " + payment);
+        }
+    }
+}
+        
+    
+
--- LogicalProgram/LogicalProgram/Program.cs
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -7,7 +7,7 @@ namespace LogicalProgram
         {
             Console.WriteLine("Welcome to Logical Program");
             Console.WriteLine("Please Choose any Option to execute Program");
-            Console.WriteLine("1:FibonacciSeries\n2:PerfectNumber\n3:PrimeNumber\n4:ReverseNumber\n5:TempConversion\n6:WeekDay");
+            Console.WriteLine("1:FibonacciSeries\n2:PerfectNumber\n3:PrimeNumber\n4:ReverseNumber\n5:TempConversion\n6:WeekDay\n7:MonthlyPayment");
             int Options = Convert.ToInt32(Console.ReadLine());
             switch(Options)
             {
@@ -29,6 +29,9 @@ namespace LogicalProgram
                 case 6:
                     WeekDay.DayOfWeek();
                     break;
+                case 7:
+                    MonthlyPayment.Payment();
+                    break;  
             }
             
 
